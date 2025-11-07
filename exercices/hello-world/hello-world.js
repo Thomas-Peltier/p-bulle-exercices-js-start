3572a1fb5c765e97181e66acdedd075440f8309a
Thomas-Peltier <pv03xgo@eduvaud.ch>
2025-11-07 14:13:13 +0100

    Feat(HelloWorld) Terminer L'exercice
    [20] [Done]

--- exercices/hello-world/hello-world.js
+++ exercices/hello-world/hello-world.js
@@ -3,6 +3,10 @@
 // Il t’est fourni pour te permettre de démarrer plus rapidement l’écriture du code.
 //
 
+let phrase = "Hello, World!"
 export function hello() {
-  throw new Error("Remove this line and implement the function");
+  return phrase;
 }
+
+
+
