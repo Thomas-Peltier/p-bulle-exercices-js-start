66056306bd20b55ed0a6d89259db8787dfc0cd6c
Thomas-Peltier <pv03xgo@eduvaud.ch>
2025-12-05 16:33:39 +0100

    feat(space-age) : terminer exercice
    [90][Done]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,47 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, ageInSeconds) => {
+  let pOrbital = [0.2408467, 0.61519726, 1.0, 1.8808158, 11.862615, 29.447498, 84.016846, 164.79132]
+  
+  if (planet === "mercury"){
+
+   return Number((ageInSeconds / (365.25 * pOrbital[0] * 60 * 60 * 24)).toFixed(2))
+     
+  }
+  else if (planet === "venus"){
+   return Number((ageInSeconds / (365.25 * pOrbital[1] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "earth"){
+   return Number((ageInSeconds / (365.25 * pOrbital[2] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "mars"){
+   return Number((ageInSeconds / (365.25 * pOrbital[3] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "jupiter"){
+   return Number((ageInSeconds / (365.25 * pOrbital[4] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "saturn"){
+   return Number((ageInSeconds / (365.25 * pOrbital[5] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "uranus"){
+   return Number((ageInSeconds / (365.25 * pOrbital[6] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "neptune"){
+   return Number((ageInSeconds / (365.25 * pOrbital[7] * 60 * 60 * 24)).toFixed(2))
+
+  }
+  else if (planet === "Sun"){
+   throw new Error("not a planet");
+   
+
+  }
+
+  
 };
