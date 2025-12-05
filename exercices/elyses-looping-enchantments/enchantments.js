a27c54ebdef1c963dc7963a8ae5b8e11b8e4af93
Thomas-Peltier <pv03xgo@eduvaud.ch>
2025-12-05 14:45:23 +0100

    feat(elyses-looping) : terminer exercice
    [90][Done]

--- exercices/elyses-looping-enchantments/enchantments.js
+++ exercices/elyses-looping-enchantments/enchantments.js
@@ -9,8 +9,15 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  // 🚨 Use .forEach
-  throw new Error('Implement the cardTypeCheck function');
+  let nbCards = 0
+
+  stack.forEach(element => {
+    if(element == card){
+      nbCards++
+
+    }
+  })
+  return nbCards
 }
 
 /**
@@ -21,6 +28,14 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  // 🚨 Use a `for...of` loop
-  throw new Error('Implement the determineOddEvenCards function');
+  let nbCards = 0
+  let odd
+  for (odd of stack)
+    if (odd % 2 == 0){
+      nbCards ++ 
+    }
+    if(!type){
+      nbCards = stack.length - nbCards
+    }
+    return nbCards
 }
