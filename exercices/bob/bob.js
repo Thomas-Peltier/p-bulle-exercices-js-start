f272d899a6e3a3af02bb59701bb2e4a66716a19f
Thomas-Peltier <pv03xgo@eduvaud.ch>
2025-12-12 14:02:19 +0100

    feat(bob) : exercice terminer
    [47][Done]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -2,7 +2,27 @@
 // This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
+ 
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+ 
+  const vide = message.trim()
+  const pattern = /([A-Za-z])/.test(vide)
+
+  if (pattern && vide === vide.toUpperCase() && vide.endsWith("?")){
+    return "Calm down, I know what I'm doing!"
+ 
+  }
+  else if(pattern && vide === vide.toUpperCase()){
+    return "Whoa, chill out!"
+  }
+  else if(vide.endsWith("?")){
+    return "Sure."
+  }
+  else if(vide === "" ){
+    return "Fine. Be that way!"
+  }
+  else {
+    return "Whatever."
+  }
 };
+ 
