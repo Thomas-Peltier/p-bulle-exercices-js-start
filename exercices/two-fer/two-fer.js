d529521f2c10f4879782fa2517da2a2bde028d6b
Thomas-Peltier <pv03xgo@eduvaud.ch>
2025-12-19 12:58:01 +0100

    feat(two-fer) : terminer exercice
    [10][Done]

--- exercices/two-fer/two-fer.js
+++ exercices/two-fer/two-fer.js
@@ -2,7 +2,14 @@
 // This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const twoFer = () => {
-  throw new Error('Remove this line and implement the function');
+ 
+export const twoFer = (nom) => {
+ 
+  if (!nom){
+    return "One for you, one for me."
+  }
+  else {
+    return `One for ${nom}, one for me.`
+  }
 };
+ 
