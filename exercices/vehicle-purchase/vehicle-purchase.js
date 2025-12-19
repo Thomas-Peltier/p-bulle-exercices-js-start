0de61da78a72f47ae434fc8253e03c363018e590
Thomas-Peltier <pv03xgo@eduvaud.ch>
2025-12-19 14:31:10 +0100

    feat(cehicle-purchase) : terminer exercice
    [5][Done]

--- exercices/vehicle-purchase/vehicle-purchase.js
+++ exercices/vehicle-purchase/vehicle-purchase.js
@@ -3,7 +3,7 @@
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
 // implementing this exercise.
-
+ 
 /**
  * Determines whether or not you need a license to operate a certain kind of vehicle.
  *
@@ -11,9 +11,9 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Remove this line and implement the function');
+  return kind === "car" || kind === "truck"
 }
-
+ 
 /**
  * Helps choosing between two options by recommending the one that
  * comes first in dictionary order.
@@ -23,9 +23,14 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Remove this line and implement the function');
+  if (option1 < option2) {
+    return `${option1} is clearly the better choice.`
+  }
+  else {
+    return `${option2} is clearly the better choice.`
+  }
 }
-
+ 
 /**
  * Calculates an estimate for the price of a used vehicle in the dealership
  * based on the original price and the age of the vehicle.
@@ -35,5 +40,14 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Remove this line and implement the function');
+  if (age < 3){
+    return originalPrice * 0.8
+  }
+  else if (age > 10){
+    return originalPrice * 0.5
+  }
+  else {
+    return originalPrice * 0.7
+  }
 }
+ 
