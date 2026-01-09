8adabde95dd32f150c04e03a4ff19888b6739a36
Thomas-Peltier <pv03xgo@eduvaud.ch>
2026-01-09 13:14:24 +0100

    feat(pizza-order) : exercice terminer
    [5][Done]

--- exercices/pizza-order/pizza-order.js
+++ exercices/pizza-order/pizza-order.js
@@ -1,7 +1,7 @@
 /// <reference path="./global.d.ts" />
 //
 // @ts-check
-
+ 
 /**
  * Determine the price of the pizza given the pizza and optional extras
  *
@@ -11,9 +11,38 @@
  * @returns {number} the price of the pizza
  */
 export function pizzaPrice(pizza, ...extras) {
-  throw new Error('Remove this line and implement the function');
+  let PrixDeBase = 0
+  switch (pizza) {
+    case 'Margherita':
+      PrixDeBase = 7
+      break
+    case 'Caprese':
+      PrixDeBase = 9
+      break
+    case 'Formaggio':
+      PrixDeBase = 10
+      break
+    default:
+      throw new Error(`Unknown pizza: ${pizza}`)
+  }
+//parcoure le tab extras et ajoute le prix
+  let prixExtra = 0
+  for (const extra of extras) {
+    switch (extra) {
+      case 'ExtraSauce':
+        prixExtra += 1
+        break
+      case 'ExtraToppings':
+        prixExtra += 2
+        break
+      default:
+        throw new Error(`Unknown extra: ${extra}`);
+    }
+  }
+ 
+  return PrixDeBase + prixExtra;
 }
-
+ 
 /**
  * Calculate the price of the total order, given individual orders
  *
@@ -24,5 +53,10 @@ export function pizzaPrice(pizza, ...extras) {
  * @returns {number} the price of the total order
  */
 export function orderPrice(pizzaOrders) {
-  throw new Error('Remove this line and implement the function');
+  //parcoure le tab pizzaOrders utilise la fonction pizzaPrice pour faire calcule prix et décomp le tab extra
+  let total = 0
+  for (const order of pizzaOrders) {
+    total += pizzaPrice(order.pizza, ...order.extras)
+  }
+  return total
 }
